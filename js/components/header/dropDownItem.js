1c3edaaf802b6e669e8fe84192bc6f892fe7885a
Ajara75 <ajararyspekova22gmail.com>
2020-10-26 16:07:55 +0600

    done

--- js/components/header/dropDownItem.js
+++ js/components/header/dropDownItem.js
@@ -2,18 +2,17 @@ import { languages } from './const.js';
 import dropDown from './style/dropdown.js';
 import { arrow } from './style/dropdown.js';
 
-let index;
+let array = [];
 
 for (let i = 0; i < languages.length; i++) {
-  index = i;
   var dropDownItems = document.createElement('li');
   dropDownItems.style.textTransform = 'uppercase';
   dropDownItems.innerHTML = `${languages[i]}`;
   dropDown.appendChild(dropDownItems);
   dropDownItems.style.transition = '0.3s';
+  array.push(dropDownItems);
   if (i > 0) {
     dropDownItems.style.opacity = '0';
-    dropDownItems.style.transform = 'translateY(-30px)';
   }
 }
 
@@ -22,18 +21,28 @@ dropDown.addEventListener('click', function () {
     dropDown.style.height = '60px';
     dropDown.className = 'none';
     dropDown.style.backgroundColor = 'transparent';
-    dropDownItems.style.opacity = '0';
-    dropDownItems.style.transform = 'translateY(-30px)';
-    arrow.style.transform = 'rotate(90deg)'
+    dropDown.style.top = '0';
+    dropDown.style.zIndex = '0';
+    array.forEach((item, index) => {
+      if (index > 0) {
+        item.style.opacity = '0';
+        item.style.visibility = 'hidden';
+      }
+    });
+    arrow.style.transform = 'rotate(90deg)';
   } else {
-    arrow.style.transform = 'rotate(-90deg)'
+    dropDown.style.zIndex = '1';
+    dropDown.style.top = '60px';
+    dropDown.style.height = 'auto';
+    arrow.style.transform = 'rotate(-90deg)';
     dropDownItems.style.display = 'block';
-    dropDownItems.style.transform = 'translateY(0)';
     dropDown.className = 'is-active';
     dropDown.style.overflow = 'visible';
     dropDown.style.backgroundColor = '#fff';
-    dropDownItems.style.opacity = '1';
+    array.forEach((item) => {
+      item.style.opacity = '1';
+      item.style.visibility = 'visible';
+    });
   }
   dropDown.style.bottom = '0';
 });
-
--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-import header from './header/index.js';
-import { imgWrapper, time } from './photoGenerator/index.js';
-import overlay from './preloader/index.js';
-
-
+import header from './components/header/index.js';
+import { imgWrapper, time } from './components/photoGenerator/index.js';
+import overlay from './components/preloader/index.js';
+import Footer from './components/footer/index.js';
 
 const root = document.getElementById('root');
-root.appendChild(overlay);
+// root.appendChild(overlay);
 root.appendChild(header);
 root.appendChild(time);
 root.appendChild(imgWrapper);
+root.appendChild(Footer());
